453a4217414ce6e233ef5db1463968cd029c684a
Nash van Gool <nash@vangool.it>
2021-12-13 21:47:28 +0100

    Day 3 puzzle 2

--- day3/src/day3.rs
+++ day3/src/day3.rs
@@ -68,3 +68,78 @@ where
 
     return PowerConsumption::from(&bit_counter);
 }
+
+pub struct LifeSupportRatings {
+    pub oxygen_rating: u64,
+    pub co2_rating: u64,
+}
+
+enum BitCriteriaType {
+    MostCommon,
+    LeastCommon
+}
+
+fn get_most_common_bit(readings: &Vec<&String>, column: usize) -> bool
+{
+    let threshold = readings.len()/2;
+    let ones_count = readings.iter()
+        .fold(
+            0,
+            |sum, reading|
+                reading.chars().nth(column).map_or(sum,
+                   |bit| if bit == '1' { sum + 1 } else { sum }
+                )
+        );
+
+    return if readings.len() % 2 == 0 { ones_count >= threshold } else { ones_count > threshold };
+}
+
+fn calculate_rating(readings: &Vec<&String>, filter_on: BitCriteriaType) -> u64
+{
+    let mut filtered = readings.clone();
+    let mut current_column = 0 as usize;
+
+    while filtered.len() > 1 {
+        let most_common_bit = get_most_common_bit(
+            &filtered,
+            current_column
+        );
+
+        filtered = filtered.into_iter()
+            .filter(|reading| {
+                let char = reading.chars().nth(current_column).unwrap();
+                let is_most_common = char == (if most_common_bit { '1' } else { '0' });
+
+                return match filter_on {
+                    BitCriteriaType::MostCommon => is_most_common,
+                    BitCriteriaType::LeastCommon => !is_most_common,
+                }
+            })
+            .collect();
+
+        current_column += 1;
+    }
+
+    return filtered.first().map_or(
+        0,
+        |reading| u64::from_str_radix(reading.as_str(), 2).unwrap_or(0)
+    );
+}
+
+pub fn calculate_life_support<'a, I>(readings: I) -> LifeSupportRatings
+where
+    I: IntoIterator<Item=&'a String>
+{
+    let readings_vector: Vec<&String> = readings.into_iter().collect();
+
+    return LifeSupportRatings {
+        oxygen_rating: calculate_rating(
+            &readings_vector,
+            BitCriteriaType::MostCommon
+        ),
+        co2_rating: calculate_rating(
+            &readings_vector,
+            BitCriteriaType::LeastCommon
+        ),
+    }
+}
--- day3/src/main.rs
+++ day3/src/main.rs
@@ -10,11 +10,18 @@ fn main() {
     ).unwrap();
 
     let power_consumption = day3::calculate_power_consumption(&readings);
+    let life_support_ratings = day3::calculate_life_support(&readings);
 
     println!(
         "Epsilon: {}, Gamma: {}, Power consumption: {}",
         power_consumption.gamma_rate,
         power_consumption.epsilon_rate,
         power_consumption.epsilon_rate * power_consumption.gamma_rate
-    )
+    );
+    println!(
+        "Oxygen rating: {}, CO2 rating: {}, Life support rating:  {}",
+        life_support_ratings.oxygen_rating,
+        life_support_ratings.co2_rating,
+        life_support_ratings.oxygen_rating * life_support_ratings.co2_rating
+    );
 }
